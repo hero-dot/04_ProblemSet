338b74a27fa2a918921542b970029d59ab70e118
hero.dot <kape.wolf@web.de>
2016-06-16 10:03:26 +0200

    final data structure

--- Task1.R
+++ Task1.R
@@ -1,193 +1,87 @@
 install.packages("rvest")
 require(rvest)
 require(magrittr)
-require(dplyr)
-library(xml2)
 
-LinkedIn <- read_html("https://www.linkedin.com/jobs/search?keywords=Data+Analyst&locationId=de:0&orig=FCTD&start=0&count=25&trk=jobs_jserp_pagination_1")
-LinkedIn1 <- read_html("https://de.linkedin.com/jobs/search?keywords=Data+Science&locationId=de:0&start=0&count=25&trk=jobs_jserp_pagination_1")
-LinkedIn2 <- read_html("https://de.linkedin.com/jobs/search?keywords=Analytics&locationId=de:0&start=0&count=25&trk=jobs_jserp_pagination_1")
-LinkedIn3 <- read_html("https://de.linkedin.com/jobs/search?keywords=Business+Intelligence&locationId=de:0&start=0&count=25&trk=jobs_jserp_pagination_1")
+# returns Data Frame out of one list
+makeDF <- function(list)
+{
+  descrList <- NULL
+  for (i in seq(1,length(list),2))
+  {
+    descrList <- rbind(descrList,list[i])
+  }
+  countList <- NULL
+  for (i in seq(2,length(list),2))
+  {
+    countList <- rbind(countList,list[i])
+  }
+  df = data.frame(descrList,countList)
+  return(df)
+}
 
 
+searches = c("Data+Analyst","Data+Science","Analytics","Business+Intelligence")
 
-pages = 44
-gesamteDaten = NULL
-company = NULL
-location = NULL
-location1 = NULL
-careerTier2 = NULL
-
-for (page in 1:pages){
+for (search in searches) 
+{
+  url <-paste0("https://de.linkedin.com/jobs/search?keywords=",search,"&locationId=de:0&start=0&count=25&trk=jobs_jserp_pagination_1")
   
-  url <- paste0("https://www.linkedin.com/jobs/search?keywords=Data+Analyst&locationId=de:0&start=",(page-1)*50,"&count=50&trk=jobs_jserp_pagination_",page)
+  htmlDoc <- read_html(url)
   
-company <- LinkedIn %>%
-  html_nodes(".jserp-results .company-name .company-name-text")%>%
-  html_text()
+  company = NULL
+  location = NULL
+  business = NULL
+  careerTier = NULL
+  
+  company <- htmlDoc %>%
+    html_nodes(".C .facet-item-link span")%>%
+    html_text()
+  
+  if (length(company)==0) 
+  {
+    company <- htmlDoc %>%
+      html_nodes(".C .facet-display-value")%>%
+      html_text()
+  }
+  
+  assign(paste0("company",search),makeDF(company)) 
+  
+  location <- htmlDoc %>%
+    html_nodes(".GC .facet-item-link span")%>%
+    html_text()
+  if (length(location)==0) 
+  {
+    location <- htmlDoc %>%
+      html_nodes(".GC .facet-display-value")%>%
+      html_text()
+  }
+  
+  assign(paste0("location",search),makeDF(location))
 
-location <- LinkedIn %>%
-  html_nodes(".jserp-results .job-location span")%>%
-  html_text()
-
-dfDataAnalyst <- data.frame(company, location)
-
-gesamteDaten = rbind(gesamteDaten,dfDataAnalyst)
-
+  business <- htmlDoc %>%
+    html_nodes(".I .facet-item-link span")%>%
+    html_text()
+  if (length(business)==0) 
+  {
+    business <- htmlDoc %>%
+      html_nodes(".I .facet-display-value")%>%
+      html_text()
+  }
+  
+  assign(paste0("business",search),makeDF(business))
+  
+  careerTier <- htmlDoc %>%
+    html_nodes(".E .facet-item-link span")%>%
+    html_text()
+  if (length(careerTier)==0) 
+  {
+    careerTier <- htmlDoc %>%
+      html_nodes(".E .facet-display-value")%>%
+      html_text()
+  }
+  
+  assign(paste0("careerTier",search),makeDF(careerTier))
 }
-
-show(gesamteDaten)
-
-
-
-
-#LinedIn (Data Analyst)
-company <- LinkedIn %>%
-  html_nodes(".C .facet-item-link span")%>%
-  html_text()
-
-company
-
-location <- LinkedIn %>%
-  html_nodes(".GC .facet-item-link span")%>%
-  html_text()
-
-location
-
-business <- LinkedIn %>%
-  html_nodes(".I .facet-item-link span")%>%
-  html_text()
-
-business
-
-careerTier <- LinkedIn %>%
-  html_nodes(".E .facet-item-link span")%>%
-  html_text()
-
-careerTier
-
-#LinkedIn1 (Data Science)
-
-company1 <- LinkedIn1 %>%
-  html_nodes(".C .facet-item-link span")%>%
-  html_text()
-
-company1
-
-location1 <- LinkedIn1 %>%
-  html_nodes(".GC .facet-item-link span")%>%
-  html_text()
-
-location1
-
-business1 <- LinkedIn1 %>%
-  html_nodes(".I .facet-item-link span")%>%
-  html_text()
-
-business1
-
-careerTier1 <- LinkedIn1 %>%
-  html_nodes(".E .facet-item-link span")%>%
-  html_text()
-
-careerTier1
-
-
-
-#LinkedIn2 (Analytics)
-
-company2 <- LinkedIn2 %>%
-  html_nodes(".C .facet-item-link span")%>%
-  html_text()
-
-company2
-
-location2 <- LinkedIn2 %>%
-  html_nodes(".GC .facet-item-link span")%>%
-  html_text()
-
-location2
-
-business2 <- LinkedIn2 %>%
-  html_nodes(".I .facet-item-link span")%>%
-  html_text()
-
-business2
-
-careerTier2 <- LinkedIn2 %>%
-  html_nodes(".E .facet-item-link span")%>%
-  html_text()
-
-careerTier2
-
-
-
-
-#LinkedIn3 (Business Intelligence)
-
-company3<- LinkedIn %>%
-  html_nodes(".C .facet-item-link span")%>%
-  html_text()
-
-company3
-
-location3 <- LinkedIn3 %>%
-  html_nodes(".GC .facet-item-link span")%>%
-  html_text()
-
-location3
-
-business3 <- LinkedIn3 %>%
-  html_nodes(".I .facet-item-link span")%>%
-  html_text()
-
-business3
-
-careerTier3 <- LinkedIn3 %>%
-  html_nodes(".E .facet-item-link span")%>%
-  html_text()
-
-careerTier3
-
-as.data.frame(t(company))
-as.data.frame(location)
-as.data.frame(business)
-as.data.frame(t(careerTier))
-
-as.data.frame(company[c(1,3,5,7,9,11,13,15,17,19)])->dfCompanyA
-as.data.frame(company[c(2,4,6,8,10,12,14,16,20)]) ->dfCompanyB
-
-dfCompanyA
-
-
-
-tierlist <- NULL
-for (i in seq(1,14,2)) 
-{
-  tierlist <- rbind(tierlist,careerTier2[i])
-}
-tieramount <- NULL
-for (i in seq(2,14,2)) 
-{
-  tieramount <- rbind(tieramount,careerTier2[i])
-}
-
-dfTier = data.frame(tierlist,tieramount)
-dfTier
-
-
-businesslist <- NULL
-for (i in seq(1,20,2))
-{
-  businesslist <- rbind(businesslist,business2[i])
-}
-
-businessamount <- NULL
-for (i in seq(2,20,2))
-{
-  businessamount <- rbind(businessamount,business2[i])
-}
-dfBusiness = data.frame(businesslist,businessamount)
 
 # a. Visualize and discuss your results.
 # Idee: Für jedes Dataframe eine Darstellung.
